379d820dc9cf2d29d3f77e3385631626bc78317d
MISSRETRO <sebegorethabile@gmail.com>
2023-07-18 20:38:42 -0700

    Adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns results
+ * @x: Value being checked
+ * @y: Value being checked
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
