85b093400c81a90ce169efe1288c614d5913953a
MISSRETRO <sebegorethabile@gmail.com>
2023-08-02 09:59:15 -0700

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns length of string
+ * @s: String to be measured
+ * Return: Length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
